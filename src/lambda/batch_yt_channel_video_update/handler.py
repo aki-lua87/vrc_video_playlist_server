36d788ea71531054426474c19bebef538c6da291
aki_lua87 <aki_lua87@outlook.jp>
2021-09-25 21:57:19 +0900

    公開用API作成

---
+++ src/lambda/batch_yt_channel_video_update/handler.py
@@ -0,0 +1,83 @@
+import os
+import time
+import urllib.request
+import json
+import boto3
+import uuid
+from boto3.dynamodb.conditions import Key
+import xml.etree.ElementTree as ET
+
+dynamodb = boto3.resource('dynamodb')
+table    = dynamodb.Table(os.environ['VRC_VIDEO_TABLE'])
+
+def main(event, context):
+    videosInfo = getUpadteChannelList()
+    if videosInfo == None:
+        print('[ERROR] not data')
+        return
+    for videoInfo in videosInfo:
+        try:
+            channel_id = videoInfo['video_id']
+            data = getData(channel_id)
+            descriptions = []
+            urls = []
+            for child in data:
+                if child.tag == '{http://www.w3.org/2005/Atom}author':
+                            for author in child:
+                                if author.tag == '{http://www.w3.org/2005/Atom}name':
+                                    name = child.find('{http://www.w3.org/2005/Atom}name').text
+                if child.tag == '{http://www.w3.org/2005/Atom}entry':
+                    for childchild in child:
+                        if childchild.tag == '{http://www.w3.org/2005/Atom}title':
+                            descriptions.append(child.find('{http://www.w3.org/2005/Atom}title').text)
+                        if childchild.tag == '{http://www.w3.org/2005/Atom}link':
+                            urls.append(childchild.attrib['href'])
+            print('[INFO] ','regist ',channel_id)
+            registVideoList(channel_id,urls,descriptions)
+            # 一覧更新フラグを立てる
+            updateChannelUpdateSuccess(channel_id,name)
+        except Exception as e:
+            print('[ERROR]',videoInfo,e)
+
+def getUpadteChannelList():
+    response = table.query(KeyConditionExpression=Key('user_id').eq('yt_channnel_id'))
+    record = response.get('Items')
+    if record == None:
+        return None
+    return record
+
+def getRssFeed(url):
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as res:
+        body = res.read().decode('utf-8')
+    return body
+
+def getData(channel_id):
+    description = []
+    target_url = []
+    url = "https://www.youtube.com/feeds/videos.xml?channel_id="+channel_id
+    body = getRssFeed(url)
+    root = ET.fromstring(body)
+    if len(root) == 0:
+        return []
+    return root
+
+def registVideoList(channel_id,video_urls,descriptions):
+    table.put_item(
+        Item={
+            'user_id': 'list_yt_ch',
+            'video_id': f'{channel_id}',
+            'titles':descriptions,
+            'urls':video_urls
+        }
+    )
+
+def updateChannelUpdateSuccess(channel_id,name):
+    table.put_item(
+        Item={
+            'user_id': 'yt_channnel_id',
+            'video_id': channel_id,
+            'author': name,
+            'index_create': True
+        }
+    )
---
+++ src/lambda/get_yt_channel_list/handler.py
@@ -0,0 +1,106 @@
+# VRCからコールするためにGET+動画をレスポンス
+from logging import fatal
+import os
+import time
+import urllib.request
+import json
+import boto3
+import uuid
+import base64
+
+dynamodb = boto3.resource('dynamodb')
+table    = dynamodb.Table(os.environ['VRC_VIDEO_TABLE'])
+
+s3 = boto3.resource('s3')
+s3_bucket = os.environ['S3_PUBLIC_BUCKET']
+
+def main(event, context):
+    channel_id = event['path'].get('channel_id')
+    isExecUpdate, isExist = GetExecVideoListCreate(channel_id)
+    if not isExist:
+        print('does not exist',event)
+        return base64.b64encode('video not exist')
+    if isExecUpdate:
+        print('create')
+        # APIの中で動画保存まで実施
+        body = call_create_video_api(channel_id)
+        # put_s3_video(s3_bucket,body,channel_id)
+        updateChannelUpdateDone(channel_id)
+    else:
+        print('s3 get')
+        body = get_s3_video(s3_bucket,channel_id)
+    return base64.b64encode(body)
+
+# リスト動画作成APIをコールし動画を取得
+def call_create_video_api(channel_id):
+    url = f'https://v9kt9fos4k.execute-api.ap-northeast-1.amazonaws.com/dev/create/video/{channel_id}'
+    print(url)
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as res:
+        body = res.read() # .decode('utf-8')
+    print(f'channel_id {channel_id} done')
+    return body
+
+# channel情報と更新有無を取得
+def GetExecVideoListCreate(channel_id):
+    response = table.get_item(
+        Key={
+            'user_id': 'yt_channnel_id',
+            'video_id': f'{channel_id}',
+        }
+    )
+    record = response.get('Item')
+    if record == None:
+        return False, False
+    isExec = record.get('index_create')
+    return isExec, True
+
+# channel動画一覧を取得
+def getVideoURLList(channel_id):
+    # Videoのlistを取得
+    v_list = GetVideoList(channel_id)
+    if v_list == None:
+        return None
+    res = []
+    for i in range(len(v_list['urls'])):
+        res.append({'urls': v_list['urls'][i],
+            'titles': v_list['titles'][i]})
+    return res
+
+def GetVideoList(channel_id):
+    response = table.get_item(
+        Key={
+            'user_id': 'list_yt_ch',
+            'video_id': f'{channel_id}',
+        }
+    )
+    record = response.get('Item')
+    if record == None:
+        return None
+    return record
+
+# 動画をS3に保存
+def put_s3_video(bucket_name,file,channel_id):
+    print('put_s3')
+    bucket = s3.Bucket(bucket_name)
+    bucket.upload_file(file, 'yt/channel/'+channel_id+'.mp4')
+
+# S3から動画を取得
+def get_s3_video(bucket_name,channel_id):
+    path = 'yt/channel/'+channel_id+'.mp4'
+    print('getS3Video',bucket_name,path)
+    bucket = s3.Bucket(bucket_name)
+    obj = bucket.Object(path)
+    response = obj.get()    
+    body = response['Body'].read()
+    return body
+
+def updateChannelUpdateDone(channel_id):
+    table.put_item(
+        Item={
+            'user_id': 'yt_channnel_id',
+            'video_id': channel_id,
+            'author': '',
+            'index_create': False
+        }
+    )
