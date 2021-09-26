5202be365b01ad47d444489a4d65369d9aaaf57a
aki_lua87 <aki_lua87@outlook.jp>
2021-09-27 02:04:31 +0900

    ログ追加＋整形

--- src/lambda/post_yt_channel_regist/handler.py
+++ src/lambda/post_yt_channel_regist/handler.py
@@ -8,17 +8,19 @@ from boto3.dynamodb.conditions import Key
 import xml.etree.ElementTree as ET
 
 dynamodb = boto3.resource('dynamodb')
-table    = dynamodb.Table(os.environ['VRC_VIDEO_TABLE'])
+table = dynamodb.Table(os.environ['VRC_VIDEO_TABLE'])
+
 
 def main(event, context):
     body = json.loads(event['body'])
+    print('body:'+body)
     channel_id = body.get('channel_id')
     # 引数チェック
     if channel_id == None:
         return {
-            'headers': { 
-                    "Access-Control-Allow-Origin": "*"
-                },
+            'headers': {
+                "Access-Control-Allow-Origin": "*"
+            },
             'statusCode': 400,
             'body': json.dumps(
                 {
@@ -27,18 +29,18 @@ def main(event, context):
             )
         }
     # channel_id登録チェック
-    isExist,auther = isExistChannelID(channel_id)
+    isExist, auther = isExistChannelID(channel_id)
     if isExist:
         # 存在する場合登録なしでOKを返却
         return {
-            'headers': { 
-                    "Access-Control-Allow-Origin": "*"
-                },
+            'headers': {
+                "Access-Control-Allow-Origin": "*"
+            },
             'statusCode': 200,
             'body': json.dumps(
                 {
                     'result': 'OK',
-                    'auther':auther
+                    'auther': auther
                 }
             )
         }
@@ -46,13 +48,13 @@ def main(event, context):
     data, isValid = getData(channel_id)
     if not isValid:
         return {
-            'headers': { 
-                    "Access-Control-Allow-Origin": "*"
-                },
+            'headers': {
+                "Access-Control-Allow-Origin": "*"
+            },
             'statusCode': 400,
             'body': json.dumps(
                 {
-                    'result':'NG',
+                    'result': 'NG',
                     'error': 'channel not exist'
                 }
             )
@@ -69,24 +71,25 @@ def main(event, context):
         if child.tag == '{http://www.w3.org/2005/Atom}entry':
             for childchild in child:
                 if childchild.tag == '{http://www.w3.org/2005/Atom}title':
-                    descriptions.append(child.find('{http://www.w3.org/2005/Atom}title').text)
+                    descriptions.append(child.find(
+                        '{http://www.w3.org/2005/Atom}title').text)
                 if childchild.tag == '{http://www.w3.org/2005/Atom}link':
                     urls.append(childchild.attrib['href'])
 
-
     # レコードを追加
     registChannel(channel_id, name)
-    registVideoList(channel_id,urls,descriptions)
+    registVideoList(channel_id, urls, descriptions)
     # https://hoge/videos/yt/{channel_id}
 
     # OK
     return {
         'headers': {
-                "Access-Control-Allow-Origin": "*"
-            },
+            "Access-Control-Allow-Origin": "*"
+        },
         'statusCode': 200,
-        'body': json.dumps({'result':'OK','auther':name}), # Auther返すこと
+        'body': json.dumps({'result': 'OK', 'auther': name}),  # Auther返すこと
     }
+
 
 def isExistChannelID(channel_id):
     response = table.get_item(
@@ -97,10 +100,11 @@ def isExistChannelID(channel_id):
     )
     isExistRecord = response.get('Item')
     if isExistRecord == None:
-        return False,""
-    return True,isExistRecord.get('author')
+        return False, ""
+    return True, isExistRecord.get('author')
 
-def registChannel(channel_id,author):
+
+def registChannel(channel_id, author):
     table.put_item(
         Item={
             'user_id': 'yt_channnel_id',
@@ -110,15 +114,17 @@ def registChannel(channel_id,author):
         }
     )
 
-def registVideoList(channel_id,video_urls,descriptions):
+
+def registVideoList(channel_id, video_urls, descriptions):
     table.put_item(
         Item={
             'user_id': 'list_yt_ch',
             'video_id': f'{channel_id}',
-            'titles':descriptions,
-            'urls':video_urls
+            'titles': descriptions,
+            'urls': video_urls
         }
     )
+
 
 def getData(channel_id):
     # ここTryCatch
@@ -129,9 +135,9 @@ def getData(channel_id):
         return [], False
     return root, True
 
+
 def getRssFeed(url):
     req = urllib.request.Request(url)
     with urllib.request.urlopen(req) as res:
         body = res.read().decode('utf-8')
     return body
-
