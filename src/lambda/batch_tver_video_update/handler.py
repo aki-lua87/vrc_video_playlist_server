1c41dd69d64d23453d20aa64736457620c5596e0
aki_lua87 <aki_lua87@outlook.jp>
2022-08-18 05:11:38 +0900

    もうなんかいろいろ対応
    - tver機能
    - ↑に伴ういろいろ追加
    - yml整理
    - Youtube見れないやつ直リンク対応暫定版

--- src/lambda/batch_tver_video_update/handler.py
+++ src/lambda/batch_tver_video_update/handler.py
@@ -3,34 +3,35 @@ import urllib.request
 import time
 import boto3
 from bs4 import BeautifulSoup
-
+from selenium import webdriver
 
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table(os.environ['VRC_VIDEO_TABLE'])
 
 tverurl = 'https://tver.jp'
+# driver = webdriver.Chrome(chrome_options=options)
 
 
 def main(event, context):
     start = time.time()
 
-    registTVer('ranking')
-    registTVer('drama')
-    registTVer('variety')
-    registTVer('documentary')
-    registTVer('anime')
-    registTVer('sport')
-    registTVer('other')
-    registTVer('c')
+    registTVer('variety', 'tags/')
+    registTVer('news_documentary', 'tags/')
+    # registTVer('variety')
+    # registTVer('documentary')
+    # registTVer('anime')
+    # registTVer('sport')
+    # registTVer('other')
+    # registTVer('c')
 
-    registTVer('short')
+    # registTVer('short')
 
-    registTVer('area-kanto', 'special/')
-    registTVer('area-kansai', 'special/')
-    registTVer('area-chubu', 'special/')
-    registTVer('area-hokkaido-tohoku', 'special/')
-    registTVer('area-chugoku-shikoku', 'special/')
-    registTVer('area-kyusyu-okinawa', 'special/')
+    # registTVer('area-kanto', 'special/')
+    # registTVer('area-kansai', 'special/')
+    # registTVer('area-chubu', 'special/')
+    # registTVer('area-hokkaido-tohoku', 'special/')
+    # registTVer('area-chugoku-shikoku', 'special/')
+    # registTVer('area-kyusyu-okinawa', 'special/')
 
     elapsed_time = time.time() - start
     print('{0}'.format(elapsed_time) + '[sec]')
@@ -48,10 +49,26 @@ def updateDB(attribute, urls, titles):
 
 
 def getHTML(url):
-    req = urllib.request.Request(url)
-    with urllib.request.urlopen(req) as res:
-        body = res.read().decode('utf-8')
-    return body
+    options = webdriver.ChromeOptions()
+    options.binary_location = '/opt/headless-chromium'
+    options.add_argument("--headless")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--hide-scrollbars")
+    options.add_argument("--single-process")
+    options.add_argument("--ignore-certificate-errors")
+    options.add_argument("--window-size=880x996")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--homedir=/tmp")
+
+    driver = webdriver.Chrome(
+        '/opt/chromedriver',
+        options=options
+    )
+    driver.get(url)
+    time.sleep(5)
+    html = driver.page_source
+    driver.quit()
+    return html
 
 
 def registTVer(attribute, urlprefix=''):
@@ -64,12 +81,16 @@ def registTVer(attribute, urlprefix=''):
     soup = BeautifulSoup(html, "html.parser")
     urls = []
     titles = []
-    for lielements in soup.select('.resumable'):
-        title = lielements.find('h3').text
-        subtitle = lielements.find('p', class_='summary').text
-        url = lielements.find('a', class_='detail_link').get('href')
-        print('タイトル:'+title + subtitle)
+    for div1elements in soup.find('div', class_='result-list_list__C6mde').children:
+        title = div1elements.find(
+            'div', class_='episode-pattern-b-layout_mainTitle__iQ_2j').text
+        print(f'タイトル:::{title}')
+        subtitle = div1elements.find(
+            'div', class_='episode-pattern-b-layout_subTitle__BnGfu').text
+        print(f'サブタイトル:::{subtitle}')
+        url = div1elements.find(
+            'a', class_='episode-pattern-b-layout_metaText__bndIm').get('href')
         print('URL:'+url)
         urls.append(f'{tverurl}{url}')
-        titles.append(f'{title} / {subtitle}')
+        titles.append(f'{title}/{subtitle}')
     updateDB(attribute, urls, titles)
